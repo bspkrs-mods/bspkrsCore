8873afa11c14e09ca23ed4a73332ff1ecdede1a0
bspkrs <bspkrs@gmail.com>
2013-12-30 23:45:22 -0500

    updated version strings

--- src/main/java/bspkrs/util/Const.java
+++ src/main/java/bspkrs/util/Const.java
@@ -2,6 +2,6 @@ package bspkrs.util;
 
 public class Const
 {
-    public static final String MCVERSION   = "1.6.4";
+    public static final String MCVERSION   = "1.7.2";
     public static final String VERSION_URL = "http://dl.dropboxusercontent.com/u/20748481";
 }
