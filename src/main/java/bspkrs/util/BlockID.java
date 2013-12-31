5d37e1a4689a48eb29bc5a8ab8d0c4233ef39f36
bspkrs <bspkrs@gmail.com>
2013-12-31 12:39:14 -0500

    fixed compiler errors, not sure if this is needed anymore

--- src/main/java/bspkrs/util/BlockID.java
+++ src/main/java/bspkrs/util/BlockID.java
@@ -2,62 +2,22 @@ package bspkrs.util;
 
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
+import bspkrs.helpers.world.WorldHelper;
 
 public class BlockID
 {
-    public int id, metadata;
+    public Block block;
+    public int   metadata;
     
-    public BlockID(int id, int metadata)
+    public BlockID(Block block, int metadata)
     {
-        this.id = id;
+        this.block = block;
         this.metadata = metadata;
-    }
-    
-    /**
-     * String must be one or two integer values delimited with the delimiter string. ex "17|0","|"
-     * 
-     * @param format
-     * @param delimiter
-     */
-    public BlockID(String format, String delimiter, int defaultVal)
-    {
-        String[] s = format.split(delimiter);
-        
-        id = CommonUtils.parseInt(s[0].trim(), defaultVal);
-        if (s.length < 2)
-            metadata = -1;
-        else
-            metadata = CommonUtils.parseInt(s[1].trim(), -1);
-    }
-    
-    public BlockID(String format, String delimiter)
-    {
-        this(format, delimiter, 0);
-    }
-    
-    /**
-     * String must be one or two integer values delimited with a comma. ex "17,0"
-     * 
-     * @param format
-     */
-    public BlockID(String format)
-    {
-        this(format, ",");
-    }
-    
-    public BlockID(net.minecraft.block.Block block, int metadata)
-    {
-        this(block.blockID, metadata);
     }
     
     public BlockID(Block block)
     {
-        this(block.blockID, -1);
-    }
-    
-    public BlockID(int id)
-    {
-        this(id, -1);
+        this(block, -1);
     }
     
     public BlockID(World world, int x, int y, int z)
@@ -67,13 +27,13 @@ public class BlockID
     
     public BlockID(World world, int x, int y, int z, int metadata)
     {
-        this(world.getBlockId(x, y, z), metadata);
+        this(WorldHelper.getBlock(world, x, y, z), metadata);
     }
     
     @Override
     public BlockID clone()
     {
-        return new BlockID(id, metadata);
+        return new BlockID(block, metadata);
     }
     
     @Override
@@ -87,16 +47,16 @@ public class BlockID
         
         BlockID o = (BlockID) obj;
         if (o.metadata == -1 || metadata == -1)
-            return id == o.id;
+            return block.equals(o.block);
         else
-            return id == o.id && metadata == o.metadata;
+            return block.equals(o.block) && metadata == o.metadata;
     }
     
     @Override
     public int hashCode()
     {
         int result = 23;
-        result = HashCodeUtil.hash(result, id);
+        result = HashCodeUtil.hash(result, block);
         result = HashCodeUtil.hash(result, metadata);
         return result;
     }
@@ -104,6 +64,6 @@ public class BlockID
     @Override
     public String toString()
     {
-        return (metadata == -1 ? id + "" : id + ", " + metadata);
+        return (metadata == -1 ? block + "" : block + ", " + metadata);
     }
 }
