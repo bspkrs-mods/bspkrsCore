ad6b78104af72457f965b183e4506924381784dd
bspkrs <bspkrs@gmail.com>
2013-12-30 23:44:17 -0500

    rewrote connection handling code

--- src/main/java/bspkrs/bspkrscore/fml/bspkrsCoreMod.java
+++ src/main/java/bspkrs/bspkrscore/fml/bspkrsCoreMod.java
@@ -1,36 +1,28 @@
 package bspkrs.bspkrscore.fml;
 
 import java.io.File;
-import java.util.EnumSet;
 
-import net.minecraft.network.INetworkManager;
-import net.minecraft.network.NetLoginHandler;
-import net.minecraft.network.packet.NetHandler;
-import net.minecraft.network.packet.Packet1Login;
-import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.client.ClientCommandHandler;
 import bspkrs.fml.util.TickerBase;
+import bspkrs.util.BSConfiguration;
 import bspkrs.util.CommonUtils;
-import bspkrs.util.Configuration;
 import bspkrs.util.Const;
 import bspkrs.util.ModVersionChecker;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.Metadata;
 import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.TickType;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.common.network.IConnectionHandler;
-import cpw.mods.fml.common.network.Player;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-@Mod(modid = "bspkrsCore", name = "bspkrsCore", version = "v5.2(" + Const.MCVERSION + ")", dependencies = "before:*", useMetadata = true)
-public class bspkrsCoreMod implements IConnectionHandler
+@Mod(modid = "bspkrsCore", name = "bspkrsCore", version = "v6.0(" + Const.MCVERSION + ")", dependencies = "before:*", useMetadata = true)
+public class bspkrsCoreMod
 {
     // config stuff
     public boolean              allowUpdateCheck          = true;
@@ -50,7 +42,7 @@ public class bspkrsCoreMod implements IConnectionHandler
     private final String        versionURL                = Const.VERSION_URL + "/Minecraft/" + Const.MCVERSION + "/bspkrsCore.version";
     private final String        mcfTopic                  = "http://www.minecraftforum.net/topic/1114612-";
     
-    private Configuration       config;
+    private BSConfiguration     config;
     
     @SideOnly(Side.CLIENT)
     protected TickerBase        ticker;
@@ -59,7 +51,7 @@ public class bspkrsCoreMod implements IConnectionHandler
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        String ctgyGen = Configuration.CATEGORY_GENERAL;
+        String ctgyGen = BSConfiguration.CATEGORY_GENERAL;
         
         metadata = event.getModMetadata();
         
@@ -71,7 +63,7 @@ public class bspkrsCoreMod implements IConnectionHandler
           //                file.delete();
         }
         
-        config = new Configuration(file);
+        config = new BSConfiguration(file);
         
         config.load();
         
@@ -97,6 +89,7 @@ public class bspkrsCoreMod implements IConnectionHandler
         
         if (event.getSide().isClient())
         {
+            FMLCommonHandler.instance().bus().register(new NetworkHandler());
             ClientCommandHandler.instance.registerCommand(new CommandBS());
             isCommandRegistered = true;
         }
@@ -107,54 +100,5 @@ public class bspkrsCoreMod implements IConnectionHandler
     {
         if (!isCommandRegistered)
             event.registerServerCommand(new CommandBS());
-    }
-    
-    /**
-     * 2) Called when a player logs into the server SERVER SIDE
-     */
-    @Override
-    public void playerLoggedIn(Player player, NetHandler netHandler, INetworkManager manager)
-    {}
-    
-    /**
-     * If you don't want the connection to continue, return a non-empty string here SERVER SIDE
-     */
-    @Override
-    public String connectionReceived(NetLoginHandler netHandler, INetworkManager manager)
-    {
-        return null;
-    }
-    
-    /**
-     * 1) Fired when a remote connection is opened CLIENT SIDE
-     */
-    @Override
-    public void connectionOpened(NetHandler netClientHandler, String server, int port, INetworkManager manager)
-    {}
-    
-    /**
-     * 1) Fired when a local connection is opened CLIENT SIDE
-     */
-    @Override
-    public void connectionOpened(NetHandler netClientHandler, MinecraftServer server, INetworkManager manager)
-    {}
-    
-    /**
-     * Fired when a connection closes ALL SIDES
-     */
-    @Override
-    public void connectionClosed(INetworkManager manager)
-    {
-        // This is probably unnecessary, but I'll leave it
-        ticker.removeTicks(EnumSet.of(TickType.CLIENT));
-    }
-    
-    /**
-     * 3) Fired when the client established the connection to the server CLIENT SIDE
-     */
-    @Override
-    public void clientLoggedIn(NetHandler clientHandler, INetworkManager manager, Packet1Login login)
-    {
-        ticker.addTicks(EnumSet.of(TickType.CLIENT));
     }
 }
