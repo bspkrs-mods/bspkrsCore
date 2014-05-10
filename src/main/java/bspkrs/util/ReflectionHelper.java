67957325b8b181bd971111a297d1d316b5fb076c
bspkrs <bspkrs@gmail.com>
2014-05-10 01:41:34 -0400

    added beginnings of QuickMod JSON file

--- src/main/java/bspkrs/util/ReflectionHelper.java
+++ src/main/java/bspkrs/util/ReflectionHelper.java
@@ -1,9 +1,62 @@
 package bspkrs.util;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 public class ReflectionHelper
 {
+    public static List getListObject(Class clazz, String srgFieldName, String mcpFieldName, Object instance)
+    {
+        try
+        {
+            Field privateField;
+            if (CommonUtils.isObfuscatedEnv())
+                privateField = clazz.getDeclaredField(srgFieldName);
+            else
+                privateField = clazz.getDeclaredField(mcpFieldName);
+            
+            privateField.setAccessible(true);
+            return (List) privateField.get(instance);
+        }
+        catch (Throwable e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static void setBooleanValue(Class clazz, String srgFieldName, String mcpFieldName, Object instance, boolean newValue) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+    {
+        Field privateField;
+        if (CommonUtils.isObfuscatedEnv())
+            privateField = clazz.getDeclaredField(srgFieldName);
+        else
+            privateField = clazz.getDeclaredField(mcpFieldName);
+        
+        privateField.setAccessible(true);
+        privateField.setBoolean(instance, newValue);
+    }
+    
+    public static boolean getBooleanValue(Class clazz, String srgFieldName, String mcpFieldName, Object instance, boolean defaultValue)
+    {
+        try
+        {
+            Field privateField;
+            if (CommonUtils.isObfuscatedEnv())
+                privateField = clazz.getDeclaredField(srgFieldName);
+            else
+                privateField = clazz.getDeclaredField(mcpFieldName);
+            
+            privateField.setAccessible(true);
+            return privateField.getBoolean(instance);
+        }
+        catch (Throwable e)
+        {
+            e.printStackTrace();
+            return defaultValue;
+        }
+    }
+    
     public static void setIntValue(Class clazz, String srgFieldName, String mcpFieldName, Object instance, int newValue) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
     {
         Field privateField;
