a7cd0d410d7f96e8f7be3864a61434dd666a66df
bspkrs <bspkrs@gmail.com>
2014-05-13 16:51:44 -0400

    fixed up config gui thing

--- src/main/java/bspkrs/bspkrscore/fml/gui/GuiBSConfig.java
+++ src/main/java/bspkrs/bspkrscore/fml/gui/GuiBSConfig.java
@@ -1,12 +1,11 @@
 package bspkrs.bspkrscore.fml.gui;
 
+import java.util.List;
+
 import net.minecraft.client.gui.GuiScreen;
-import bspkrs.bspkrscore.fml.ConfigElement;
 import bspkrs.bspkrscore.fml.Reference;
-import bspkrs.util.config.ConfigCategory;
 import bspkrs.util.config.ConfigProperty;
 import bspkrs.util.config.Configuration;
-import bspkrs.util.config.Property;
 import bspkrs.util.config.gui.GuiConfig;
 import bspkrs.util.config.gui.IConfigProperty;
 
@@ -14,23 +13,13 @@ public class GuiBSConfig extends GuiConfig
 {
     public GuiBSConfig(GuiScreen parent) throws NoSuchMethodException, SecurityException
     {
-        super(parent, getProps(), true, "bspkrsCore", true, GuiConfig.getAbridgedConfigPath(Reference.config.toString()));
+        super(parent, getProps(), true, Reference.MODID, true, GuiConfig.getAbridgedConfigPath(Reference.config.toString()));
     }
     
-    private static IConfigProperty[] getProps()
+    private static List<IConfigProperty> getProps()
     {
-        ConfigCategory cc = Reference.config.getCategory(Configuration.CATEGORY_GENERAL);
-        IConfigProperty[] props = new IConfigProperty[ConfigElement.values().length + 1];
-        for (int i = 0; i < ConfigElement.values().length; i++)
-        {
-            ConfigElement ce = ConfigElement.values()[i];
-            Property prop = cc.get(ce.key());
-            if (prop != null)
-                props[i] = new ConfigProperty(prop, ce.propertyType());
-        }
-        
-        props[props.length - 1] = new ConfigProperty(Reference.config.getCategory(Configuration.CATEGORY_GENERAL + ".example_properties"));
-        
+        List<IConfigProperty> props = (new ConfigProperty(Reference.config.getCategory(Configuration.CATEGORY_GENERAL))).getConfigPropertiesList(true);
+        props.add(new ConfigProperty(Reference.config.getCategory(Configuration.CATEGORY_GENERAL + ".example_properties")));
         return props;
     }
 }
