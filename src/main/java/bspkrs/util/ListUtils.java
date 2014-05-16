e35278a27ed37d26730c6bce19dbdd996418fb5b
bspkrs <bspkrs@gmail.com>
2014-05-16 02:28:40 -0400

    added more ListUtils methods

--- src/main/java/bspkrs/util/ListUtils.java
+++ src/main/java/bspkrs/util/ListUtils.java
@@ -15,13 +15,38 @@ public class ListUtils
         return r.replaceFirst(delimiter, "");
     }
     
+    public static String getListAsUniqueDelimitedString(List<?> list, String delimiter)
+    {
+        String r = "";
+        
+        List seen = new ArrayList();
+        
+        for (Object o : list)
+            if (!seen.contains(o))
+            {
+                r += delimiter + o.toString();
+                seen.add(o);
+            }
+        
+        return r.replaceFirst(delimiter, "");
+    }
+    
+    public static boolean doesListAContainAllUniqueListBValues(List<?> listA, List<?> listB)
+    {
+        for (Object o : listB)
+            if (!listA.contains(o))
+                return false;
+        
+        return true;
+    }
+    
     public static List<BlockID> getDelimitedStringAsBlockIDList(String dList, String delimiter)
     {
         List<BlockID> list = new ArrayList<BlockID>();
         
         for (String format : dList.split(delimiter))
             if (!format.trim().isEmpty())
-                list.add(new BlockID(format, ","));
+                list.add(BlockID.parse(format));
         
         return list;
     }
