58088b5f1f03b6c253c7697782a9209d8cc05d3e
bspkrs <bspkrs@gmail.com>
2014-05-23 13:04:37 -0400

    Revert "added exceptions"
    This reverts commit e511c1ae96d074b7ab7d6240662e779522d208b2.

--- src/main/java/bspkrs/network/BSPacket.java
+++ src/main/java/bspkrs/network/BSPacket.java
@@ -1,12 +1,10 @@
 package bspkrs.network;
 
-import java.io.IOException;
-
 import net.minecraft.network.PacketBuffer;
 
 public interface BSPacket
 {
-    public void readBytes(PacketBuffer bytes) throws IOException;
+    public void readBytes(PacketBuffer bytes);
     
-    public void writeBytes(PacketBuffer bytes) throws IOException;
+    public void writeBytes(PacketBuffer bytes);
 }
