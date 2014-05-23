e511c1ae96d074b7ab7d6240662e779522d208b2
bspkrs <bspkrs@gmail.com>
2014-05-23 13:02:41 -0400

    added exceptions

--- src/main/java/bspkrs/network/BSPacket.java
+++ src/main/java/bspkrs/network/BSPacket.java
@@ -1,10 +1,12 @@
 package bspkrs.network;
 
+import java.io.IOException;
+
 import net.minecraft.network.PacketBuffer;
 
 public interface BSPacket
 {
-    public void readBytes(PacketBuffer bytes);
+    public void readBytes(PacketBuffer bytes) throws IOException;
     
-    public void writeBytes(PacketBuffer bytes);
+    public void writeBytes(PacketBuffer bytes) throws IOException;
 }
