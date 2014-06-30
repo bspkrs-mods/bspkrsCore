cd743224cb9399ec7f447308dc050195aaad10ae
bspkrs <bspkrs@gmail.com>
2014-06-30 01:02:30 -0400

    trying bspk.rs URL

--- src/main/java/bspkrs/util/Const.java
+++ src/main/java/bspkrs/util/Const.java
@@ -3,5 +3,5 @@ package bspkrs.util;
 public class Const
 {
     public static final String MCVERSION   = "@MINECRAFT_VERSION@";
-    public static final String VERSION_URL = "http://dl.dropboxusercontent.com/u/20748481";
+    public static final String VERSION_URL = "http://bspk.rs";
 }
